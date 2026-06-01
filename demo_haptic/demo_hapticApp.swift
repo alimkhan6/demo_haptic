5bb4a9095eb2a3c98719a7b37cbd6786d8b94a0e
alimkhan6 <descdway@icloud.com>
2026-06-01 19:37:03 +0500

    Initial Commit

---
+++ demo_haptic/demo_hapticApp.swift
@@ -0,0 +1,17 @@
+//
+//  demo_hapticApp.swift
+//  demo_haptic
+//
+//  Created by Alimkhan Zhanbekov on 01.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct demo_hapticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
